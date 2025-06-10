4e83049764f9a4f58f817cec8897c84d6b2a337e
kshksh723 <kshksh723@naver.com>
2025-06-10 11:41:23 +0900

    arithmetic operator

---
+++ FRONTEND/Try_HelloWorldJS/lecture10.js
@@ -0,0 +1,41 @@
+// a라는 변수를 초기화하고 a, a+1, a를 출력ㄷ하는 코드를 작성해보기
+var a = 1;
+console.log(a); // 1
+console.log(a + 1); // 2
+console.log(a); // 1
+
+console.log("============");
+// ++ 연산자로 바꾸기
+
+var b = 1;
+console.log(b); // 1
+console.log(++b); // 2 ▶️ ++ 연산자로 수정
+console.log(b); // 2
+
+console.log("============");
+
+var c = 1;
+console.log(c); // 1
+console.log(--c); // 0 ▶️ -- 연산자로 수정
+console.log(c); // 0
+
+console.log("============");
+
+var d = 1;
+console.log(d); // 1
+console.log(d++); // 1  ++ 연산자를 변수 뒤에 썼을 때
+console.log(d); // 2
+
+console.log("========");
+
+var a1 = 10;
+var b1 = 7;
+
+// c1는 a와 b의 합
+var c1 = a1 + b1;
+
+// d1는 a와 b의 차
+var d1 = a1 - b1;
+
+// e1는 a와 b의 곱
+var e1 = a * b;
